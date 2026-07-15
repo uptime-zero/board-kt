3081388c5488af58229560bd7826c1fdf1030c30
uptime-zero <dereckjun2173@gmail.com>
2026-07-16 00:05:41 +0900

    feat: 게시글 페이지 전체 조회 API 구현
    - 커스텀 Page 클래스(=PaginatedResponse) 설계 후 사용
    - 기본값: page=1, size=10, 생성시간 내림차순 설정

--- src/main/kotlin/com/boardkt/post/controller/PostController.kt
+++ src/main/kotlin/com/boardkt/post/controller/PostController.kt
@@ -1,15 +1,20 @@
 package com.boardkt.post.controller
 
+import com.boardkt.global.dto.PaginatedResponse
 import com.boardkt.post.dto.CreatePostRequest
 import com.boardkt.post.dto.PostResponse
 import com.boardkt.post.service.PostService
 import jakarta.validation.Valid
 import lombok.RequiredArgsConstructor
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -27,5 +32,14 @@ class PostController(
     fun getSinglePost(@PathVariable id: Long): ResponseEntity<PostResponse> {
         val post = postService.getSinglePost(id)
         return ResponseEntity.ok(post)
+    }
+
+    @GetMapping("/posts")
+    fun getAllPostsToPage(
+        @PageableDefault(page = 0, size = 10, sort = ["createdAt"], direction = Sort.Direction.DESC)
+        pageable: Pageable
+    ): ResponseEntity<PaginatedResponse<PostResponse>> {
+        val postsToPage = postService.getAllPostsToPage(pageable)
+        return ResponseEntity.ok(postsToPage)
     }
 }
--- src/main/kotlin/com/boardkt/post/repository/PostJpaRepository.kt
+++ src/main/kotlin/com/boardkt/post/repository/PostJpaRepository.kt
@@ -1,8 +1,10 @@
 package com.boardkt.post.repository
 
 import com.boardkt.post.entity.Post
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostJpaRepository: JpaRepository<Post, Long> {
-
+interface PostJpaRepository : JpaRepository<Post, Long> {
+    override fun findAll(pageable: Pageable): Page<Post>
 }
