087777a75921beea9173641ebe77bd3478da8a23
uptime-zero <dereckjun2173@gmail.com>
2026-07-15 21:06:10 +0900

    feat: 게시글 단건 조회 API 구현
    - @PathVariable을 통해 post id로 엔드포인트에 접근하도록 함
    - 해당하는 게시글이 없을 경우 예외 발생
       - 예외 처리 핸들링은 아직 되어있지 않음
    - PostResponse를 반환함

--- src/main/kotlin/com/boardkt/post/controller/PostController.kt
+++ src/main/kotlin/com/boardkt/post/controller/PostController.kt
@@ -6,6 +6,8 @@ import com.boardkt.post.service.PostService
 import jakarta.validation.Valid
 import lombok.RequiredArgsConstructor
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -19,5 +21,11 @@ class PostController(
     fun createPost(@Valid @RequestBody request: CreatePostRequest): ResponseEntity<PostResponse> {
         val newPost = postService.createPost(request.title, request.content)
         return ResponseEntity.status(201).body(newPost)
+    }
+
+    @GetMapping("/posts/{id}")
+    fun getSinglePost(@PathVariable id: Long): ResponseEntity<PostResponse> {
+        val post = postService.getSinglePost(id)
+        return ResponseEntity.ok(post)
     }
 }
--- src/main/kotlin/com/boardkt/post/service/PostService.kt
+++ src/main/kotlin/com/boardkt/post/service/PostService.kt
@@ -5,6 +5,7 @@ import com.boardkt.post.entity.Post
 import com.boardkt.post.repository.PostJpaRepository
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -18,5 +19,11 @@ class PostService(
     fun createPost(title: String, content: String): PostResponse {
         val newPost = postJpaRepository.save(Post(title = title, content = content))
         return PostResponse(newPost.id!!, newPost.title, newPost.content)
+    }
+
+    @Transactional(readOnly = true)
+    fun getSinglePost(id: Long): PostResponse {
+        val getPost = postJpaRepository.findByIdOrNull(id)?: throw RuntimeException("해당 게시글을 찾을 수 없습니다.")
+        return PostResponse(getPost.id!!, getPost.title, getPost.content)
     }
 }
