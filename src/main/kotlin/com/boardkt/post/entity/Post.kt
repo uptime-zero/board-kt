84d5d226274863722dabc071d0795993484ae87a
uptime-zero <dereckjun2173@gmail.com>
2026-07-15 19:59:32 +0900

    refactor: @Table 어노테이션 추가 및 import 정리

--- src/main/kotlin/com/boardkt/post/entity/Post.kt
+++ src/main/kotlin/com/boardkt/post/entity/Post.kt
@@ -1,16 +1,13 @@
 package com.boardkt.post.entity
 
 import com.boardkt.global.entity.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import lombok.Getter
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "posts")
 class Post(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     var title: String,
     var content: String,
-): BaseEntity()
+) : BaseEntity()
