aa0ab671675ccb16ba6e0aae5d8f02b3d4390c03
uptime-zero <dereckjun2173@gmail.com>
2026-07-16 13:30:46 +0900

    feat: 게시글 일부 수정 API 구현
    - `@PathVariable`을 통해 게시글 `id`로 엔드포인트에 접근하도록 함
    - 해당하는 게시글이 없을 경우 예외 발생
       - 예외 처리 핸들링 미구현
    - 제목, 본문 모두 Null 또는 공백일 경우 수정 불가
    - `Post`에 객체 수정 로직 `patch()` 추가

--- src/main/kotlin/com/boardkt/post/controller/PostController.kt
+++ src/main/kotlin/com/boardkt/post/controller/PostController.kt
@@ -2,6 +2,7 @@ package com.boardkt.post.controller
 
 import com.boardkt.global.dto.PaginatedResponse
 import com.boardkt.post.dto.CreatePostRequest
+import com.boardkt.post.dto.PatchPostRequest
 import com.boardkt.post.dto.PostResponse
 import com.boardkt.post.service.PostService
 import jakarta.validation.Valid
@@ -10,12 +11,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequiredArgsConstructor
@@ -41,5 +37,14 @@ class PostController(
     ): ResponseEntity<PaginatedResponse<PostResponse>> {
         val postsToPage = postService.getAllPostsToPage(pageable)
         return ResponseEntity.ok(postsToPage)
+    }
+
+    @PatchMapping("/posts/{id}")
+    fun patchPost(
+        @PathVariable id: Long,
+        @RequestBody request: PatchPostRequest
+    ): ResponseEntity<PostResponse> {
+        val patchedPost = postService.patchPost(id, request.title, request.content)
+        return ResponseEntity.ok(patchedPost)
     }
 }
